df1e6e38175fb3c8844e16305133adb7a0039455
obahdoh <oab.3.14.159@gmail.com>
2025-03-16 20:33:02 +0000

    started on dragon project

---
+++ dragon/main.py
@@ -0,0 +1,23 @@
+import tinkter *
+tk = TK()
+
+def start
+canvas = Canvas(tk, width=500 height=700)
+canvas.create_text(text = '
+
+                                      Chapter one
+
+
+
+Griffin was a short blonde-haired boy who was friends with a girl called Clara and a boy called Rory.
+
+
+“Griffin!” called his Mum, “Come downstairs!”
+“Coming Mum!” he yelled back.
+Griffin finished getting dressed, rushed downstairs, wolfed down his breakfast, and waved his Mum goodbye as he left for school. As he walked happily down his driveway, he messaged his friends a quick note to meet him in the park before school; he was hoping they would meet him in time for a quick game before the bell.  As he spotted his friends already ahead of him waiting  in the park, he grinned and yelled out to them.
+”Come on. Let/'s play tag!”
+“Okay,” Ayzen said “You're it.”
+Griffin ran after Clara “You're it.”
+“No,” she replied “You/'re it.”'font='Helvetica',20 ')0
+
+btn= 
