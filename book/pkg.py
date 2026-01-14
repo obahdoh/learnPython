100eb0a24b37bc2cdddd85d174f27088c9048687
obahdoh <oab.3.14.159@gmail.com>
2026-01-14 19:05:28 +0000

    hello

--- book/pkg.py
+++ book/pkg.py
@@ -1,3 +1,4 @@
-ingredinets=['snails','leeches','']
+ingredients=['snails','leeches','gorilla belly-button lint','caterpillar eyebrows','centipede toes']
+owen=0
 for x in range (1,5):
-    print(x, )
+    print(x, in)
